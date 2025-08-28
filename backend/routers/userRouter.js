0d4cce9b6faf2461596643d8ebd034da54192e73
melaku2402 <melakuadane004@gmail.com>
2025-08-28 21:39:55 +0300

    Update userRouter.js

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -2,9 +2,6 @@ import React from 'react'
 import express from 'express'
 import { loginUser, registerUser } from '../controllers/userController.js'
 
-// import { Router } from "express"; // Use ES Module import syntax
-// const userRouter = Router();
-
  const userRouter = express.Router()
 
 userRouter.post("/register",registerUser);
